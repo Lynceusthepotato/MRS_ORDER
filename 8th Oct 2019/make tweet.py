509429b17d8aed3a8217fe1b78beb789929afa85
Lynceusthepotato <lukman.hanafi@outlook.com>
2019-10-09 11:09:51 +0700

    update?
    idk

--- 8th Oct 2019/make tweet.py
+++ 8th Oct 2019/make tweet.py
@@ -5,6 +5,10 @@ PC = []
 with open(filename) as f:
     txt = json.load(f)
     PC = txt["statuses"]
+def loadtweet(YN):
+    if YN == "Y":
+        for i in range(0,3):
+            print(PC[i]["text"])
 
 def findid(chat):
     for i in PC:
@@ -13,6 +17,13 @@ def findid(chat):
         elif i not in PC:
             print("There is no id")
 
+'''def findtext(chat): #Doesnt work 
+    for i in range(0,len(PC)):
+        if PC[i]["text"] == chat:
+            return i
+        elif i not in PC:
+            print("There is no thread")'''
+
 def savethread(thread):
     PC.append(thread)
 
@@ -20,7 +31,10 @@ def savefile():
     with open(filename,"w") as f:
         json.dump({"text": PC}, f)
 
-id = input("Enter an id: ") # put in id_str
+
+tweet = input("Do you want to load tweet [Y/N]: ")
+loadtweet(tweet.capitalize())
+id = input("Enter a id: ")# enter id_str
 userchoice = findid(id)
 if userchoice is not None:
     print(userchoice)
@@ -32,4 +46,3 @@ else:
     savethread(newusername)
     savethread(newopinion)
     print(PC)
-    savefile()
